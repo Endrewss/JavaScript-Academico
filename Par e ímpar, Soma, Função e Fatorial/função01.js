b511534966d96b3aadc19302a3b9e54a7d331a0f
Endrews Azevedo <98989141+Endrewss@users.noreply.github.com>
2023-03-06 09:52:50 -0300

    Add Files Js

---
+++ Par e ímpar, Soma, Função e Fatorial/função01.js
@@ -0,0 +1,12 @@
+function parimpar(n) {
+    if (n % 2 == 0) {
+        return "Par!"
+    } else {
+        return "Ímpar!"
+    }
+}
+
+let res = parimpar(4)
+console.log(res)
+
+console.log(parimpar(223))
---
+++ Par e ímpar, Soma, Função e Fatorial/função05.js
@@ -0,0 +1,9 @@
+function fatorial(n) {
+    if(n == 1){
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
+}
+
+console.log(fatorial(5))
