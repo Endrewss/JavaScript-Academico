7ad1b43c141312dd22de929726c88ad3af7656e2
Endrews Azevedo <98989141+Endrewss@users.noreply.github.com>
2023-03-09 08:58:49 -0300

    Add Files Exercise While and Do While

---
+++ While e Do While/Ambiente01.js
@@ -0,0 +1,6 @@
+console.log("Tudo bem?")
+var c = 1
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++
+}
---
+++ While e Do While/ambiente02.js
@@ -0,0 +1,5 @@
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 3)
