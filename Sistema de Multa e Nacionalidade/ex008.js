a6e7e0edc50447a36d33568151ce9c1a8fb0b9ea
Endrews Azevedo <98989141+Endrewss@users.noreply.github.com>
2023-03-10 09:42:23 -0300

    Add Files Exercises SistemaMulta e Nacionalidade

---
+++ Sistema de Multa e Nacionalidade/ex008.js
@@ -0,0 +1,8 @@
+var vel = 15
+console.log(`A velocidade é do seu carro é ${vel}km/h!`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Diriga sempre usando cinto de segurança!`)
+
+//Condição Simples
---
+++ Sistema de Multa e Nacionalidade/ex009.js
@@ -0,0 +1,9 @@
+var país = "EUA"
+console.log (`Vivendo em ${país}`)
+if (país == "brasil"){
+    console.log ("Você é Brasileiro!")
+} else {
+    console.log ("Você é Gringo!")
+}
+
+//Condição Composta
